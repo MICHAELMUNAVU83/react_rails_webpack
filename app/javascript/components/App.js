9f5b2872da6c3d13c2f9337509c9848f8698e9e9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-24 13:05:18 +0300

    Changed readme file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Greeting from "./Greeting";
 import { Provider } from "react-redux";
 import store from "../configureStore";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Greeting />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
